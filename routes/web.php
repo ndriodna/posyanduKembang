c687cbed311dd2941abf45468fb6af58a217b09c
ndriodna <kurodesu6969@gmail.com>
2021-03-16 21:28:19 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,12 @@ Route::group(['middleware' => 'auth'], function () {
 
 	Route::get('/home', 'HomeController@index')->name('home');
 	Route::get('/dashboard-user', 'HomeController@user');
-<<<<<<< HEAD
-
-	Route::group(['middleware' => ['role:Admin']], function (){
-
-=======
 	Route::get('/user/show','UserController@showAuthUser')->name('user.showAuthUser');
+
 	Route::group(['middleware' => ['role:Admin']], function (){
-  
->>>>>>> ebf4c70f6124bc062e29684ccced59b9b6ca2e1b
 	  Route::resource('residents', 'ResidentController');
 		Route::resource('user', 'UserController');
 		Route::resource('roles', 'RoleController');
-		
 	});
 
 });
