be54a5a68b57e650bd2dea0ef3815702c28f844d
ndriodna <kurodesu6969@gmail.com>
2021-03-18 15:20:44 +0800

    login change to stisla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,92 +1,64 @@
-@extends('layouts.app', ['class' => 'bg-default'])
+@extends('layouts.app')
 
 @section('content')
-    @include('layouts.headers.guest')
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+      <div class="login-brand">
+        <img src="../assets/stisla.svg" alt="logo" width="100" class="shadow-light rounded-circle bg-success">
+      </div>
 
-    <div class="container mt--8 pb-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-5 col-md-7">
-                <div class="card bg-secondary shadow border-0">
-                    <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
-                        <div class="btn-wrapper text-center">
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
-                                <span class="btn-inner--text">{{ __('Github') }}</span>
-                            </a>
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
-                                <span class="btn-inner--text">{{ __('Google') }}</span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="card-body px-lg-5 py-lg-5">
-                        <div class="text-center text-muted mb-4">
-                            <small>
-                                <a href="{{ route('register') }}">{{ __('Create new account') }}</a> {{ __('OR Sign in with these credentials:') }}
-                            </small>
-                            <br>
-                            <small>
-                                {{ __('Username') }} <strong>admin@argon.com</strong>
-                                {{ __('Password') }} <strong>secret</strong>
-                            </small>
-                        </div>
-                        <form role="form" method="POST" action="{{ route('login') }}">
-                            @csrf
+      <div class="card card-primary">
+        <div class="card-header"><h4>Login</h4></div>
 
-                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-3">
-                                <div class="input-group input-group-alternative">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email') }}" value="admin@argon.com" required autofocus>
-                                </div>
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                                <div class="input-group input-group-alternative">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                                    </div>
-                                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" value="secret" required>
-                                </div>
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="custom-control custom-control-alternative custom-checkbox">
-                                <input class="custom-control-input" name="remember" id="customCheckLogin" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label" for="customCheckLogin">
-                                    <span class="text-muted">{{ __('Remember me') }}</span>
-                                </label>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-6">
-                        @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}" class="text-light">
-                                <small>{{ __('Forgot password?') }}</small>
-                            </a>
-                        @endif
-                    </div>
-                    <div class="col-6 text-right">
-                        <a href="{{ route('register') }}" class="text-light">
-                            <small>{{ __('Create new account') }}</small>
-                        </a>
-                    </div>
-                </div>
+        <div class="card-body">
+          <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+            @csrf
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+              <div class="invalid-feedback">
+                Please fill in your email
+              </div>
             </div>
+
+            <div class="form-group">
+              <div class="d-block">
+                <label for="password" class="control-label">Password</label>
+                <div class="float-right">
+                  <a href="auth-forgot-password.html" class="text-small">
+                    Forgot Password?
+                  </a>
+                </div>
+              </div>
+              <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+              <div class="invalid-feedback">
+                please fill in your password
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                <label class="custom-control-label" for="remember-me">Remember Me</label>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
+      <div class="mt-5 text-muted text-center">
+        Don't have an account? <a href="{{route('register')}}">Create One</a>
+      </div>
+      <div class="simple-footer">
+        Copyright &copy; Stisla 2018
+      </div>
     </div>
+  </div>
+</div>
 @endsection
--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,4 +1,4 @@
-@include('layouts.navbars.navs.guest')
+{{-- @include('layouts.navbars.navs.guest') --}}
   <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
     @yield('content')
     @include('layouts.footers.guest')
