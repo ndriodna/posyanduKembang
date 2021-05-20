ea5bc639b4e287d6597b87eca1bad65edb564a48
eeQ <kurodesu6969@gmail.com>
2021-05-20 22:19:38 +0800

    icon sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -24,10 +24,21 @@
                 </ul>
               </li>
               @role('Admin')
-              <li class=""><a class="nav-link" href="{{route('pendaftaran.index')}}"><i class="far fa-square"></i> <span>Pendaftaran</span></a></li>
-              <li class=""><a class="nav-link" href="{{route('pencatatan.index')}}"><i class="far fa-square"></i> <span>Pencatatan</span></a></li>
+              <li class=""><a class="nav-link" href="{{route('pendaftaran.index')}}"><i class="fas fa-edit"></i> <span>Pendaftaran</span></a></li>
+              <li class=""><a class="nav-link" href="{{route('pencatatan.index')}}"><i class="fas fa-pen"></i> <span>Pencatatan</span></a></li>
               <li class=""><a class="nav-link" href="{{route('penyuluhan.index')}}"><i class="far fa-square"></i> <span>Penyuluhan</span></a></li>
-              <li class=""><a class="nav-link" href="{{route('pelayanan.index')}}"><i class="far fa-square"></i> <span>Pelayanan</span></a></li>
+              {{-- <li class=""><a class="nav-link" href="{{route('pelayanan.index')}}"><i class="far fa-square"></i> <span>Pelayanan</span></a></li> --}}
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-pills"></i> <span>Pelayanan</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="{{route('user.showAuthUser')}}">Daftar</a></li>
+                  @role('Admin')
+                  <li><a class="nav-link" href="{{route('pelayanan.index')}}">Tambah </a></li>
+                  @endrole
+                </ul>
+              </li>
+              <li class=""><a class="nav-link" href="{{route('penyuluhan.index')}}"><i class="fas fa-file-alt"></i> <span>Rekap</span></a></li>
+
               @endrole
               <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Item</span></a>
